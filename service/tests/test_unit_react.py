55d8151d8b8964aaa60a93091e978e2bb7be913c
OrnelaDanushi <orneladanushi@gmail.com>
2019-11-17 11:59:47 +0100

    added get test in test_unit_react.oy

--- service/tests/test_unit_react.py
+++ service/tests/test_unit_react.py
@@ -4,6 +4,10 @@ MOCK_ACCESS_TOKEN = 'csrf_access_token=01573355-85b5-4688-b74c-1efa0e5c08d2'
 MOCK_REACT_BADTYPE = {'react': 10}
 MOCK_REACT_BADVALUE = {'react': 'laic'}
 
+MOCK_GET_USER_REACT = {'user_id': 'testuserid'}
+MOCK_GET_STORY_REACT = {'story_id': 'teststoryid'}
+
+
 def test_react_success(app, client, reactions, requests_mock):
     auth.client = client
 
@@ -50,3 +54,53 @@ def test_react_twice(app, client):
     assert reply.get_json().message == 'you\'ve already disliked this story!'
 
 #TODO: test get
+
+def test_get_user_react_success(app, client, reactions, requests_mock):
+    reactions.client = client
+
+    data = MOCK_GET_USER_REACT.copy()
+    data_user_id = data['user_id']
+
+    requests_mock.get(f'{app.config["USERS_ENDPOINT"]}/api/users/{data_user_id}/get_react',
+                      status_code=200)
+
+    reply = reactions.get_user_react(MOCK_GET_USER_REACT)
+    assert reply.status_code == 200
+
+def test_get_user_react_failure(app, client, reactions, requests_mock):
+    reactions.client = client
+
+    data = MOCK_GET_USER_REACT.copy()
+    data_user_id = data['user_id']
+
+    requests_mock.get(f'{app.config["USERS_ENDPOINT"]}/api/users/{data_user_id}/get_react',
+                      status_code=404)
+
+    reply = reactions.get_user_react(MOCK_GET_USER_REACT)
+    assert reply.status_code == 404
+    assert reply.get_json().code == 'E112' #unregistered user
+
+def test_get_story_react_success(app, client, reactions, requests_mock):
+    reactions.client = client
+
+    data = MOCK_STORY_REACT.copy()
+    data_story_id = data['story_id']
+
+    requests_mock.get(f'{app.config["USERS_ENDPOINT"]}/api/users/{data_story_id}/get_react',
+                      status_code=200)
+
+    reply = reactions.get_story_react(MOCK_STORY_REACT)
+    assert reply.status_code == 200
+
+def test_get_story_react_failure(app, client, reactions, requests_mock):
+    reactions.client = client
+
+    data = MOCK_STORY_REACT.copy()
+    data_story_id = data['story_id']
+
+    requests_mock.get(f'{app.config["USERS_ENDPOINT"]}/api/users/{data_story_id}/get_react',
+                      status_code=404)
+
+    reply = reactions.get_story_react(MOCK_STORY_REACT)
+    assert reply.status_code == 404
+    assert reply.get_json().code == 'unregistered story' #to add the error case in errors.py
