6404f05a579954ccf2821d2a691df10cd34a4e23
Laura <l.bussi1@studenti.unipi.it>
2019-11-24 14:45:24 +0100

    removed useless file

--- service/definitions.py
+++
@@ -1,7 +0,0 @@
-import os
-from py_config import set_config_dir, get_config
-
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-config_dir = os.path.abspath(os.path.dirname(__file__))
-set_config_dir(config_dir)
-config = get_config()
