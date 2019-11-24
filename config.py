983d1c1a6602130216f71fb2b3803f5269724736
Laura <l.bussi1@studenti.unipi.it>
2019-11-24 11:46:12 +0100

    updated config

--- config.py
+++ config.py
@@ -21,3 +21,12 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_DISABLE_RATE_LIMITS = True
 CELERY_ACCEPT_CONTENT = ['json']
+
+#jwt
+SECRET_KEY = 'some-secret-string-CHANGE-ME'
+JWT_SECRET_KEY = 'jwt-secret-string-CHANGE-ME'
+JWT_TOKEN_LOCATION = ['cookies']
+JWT_ACCESS_COOKIE_PATH = '/'
+JWT_REFRESH_COOKIE_PATH = '/auth/token_refresh'
+JWT_COOKIE_CSRF_PROTECT = False
+JWT_COOKIE_SECURE = False
